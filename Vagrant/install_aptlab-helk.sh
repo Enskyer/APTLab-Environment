208b9786c0b7a31db3d40701e1e11bd0d2099152
Enskyer <josua0923@gmail.com>
2023-04-16 02:29:13 +0800

    Change virtual machine configuration.

--- Vagrant/install_aptlab-helk.sh
+++ Vagrant/install_aptlab-helk.sh
@@ -9,7 +9,7 @@ install_APTLab_HELK() {
   export DEBIAN_FRONTEND=noninteractive 
   apt-get install -y apache2-utils
   cd /opt
-  git clone https://github.com/stavhaygn/APTLab-HELK.git
+  git clone https://github.com/Enskyer/APTLab-HELK.git
   cd /opt/APTLab-HELK/docker/ && ./helk_install.sh -p $HELK_KIBANA_UI_PASSWORD -n $HELK_NEO4J_PASSWORD -i $HOST_IP -b 'aptlab-helk-kibana-notebook-neo4j-analysis'
 }
 
--- Vagrant/install_mitre_caldera.sh
+++ Vagrant/install_mitre_caldera.sh
@@ -1,9 +1,9 @@
 #! /usr/bin/env bash
 
 install_mitre_caldera() {
-  echo "[$(date +%H:%M:%S)]: Installing stavhaygn/mitre-caldera..."
+  echo "[$(date +%H:%M:%S)]: Installing Enskyer/mitre-caldera..."
   cd /opt
-  git clone https://github.com/stavhaygn/mitre-caldera.git --recursive --branch 4.0.0.20220421-beta
+  git clone https://github.com/Enskyer/caldera.git --recursive --branch 4.1.0.20230328
   cd /opt/mitre-caldera
   docker compose up -d --build
   # Need to up again, because the caldera has some problems when it up for the first time
